c13069da86720ae64242b13bdbb3a46a53a688ae
seshna <storkyd@gmail.com>
2015-08-02 10:51:27 +0800

    Initial Repository

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+  "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+  <title>Under Construction</title>
+
+  <style type="text/css">
+  <!--
+  .style1 {
+   letter-spacing: -1pt;
+   color: #154B7A;
+ }
+ .container {
+   height: 400px;
+   width: auto;
+   margin-right: auto;
+   margin-left: auto;
+ }
+ body {
+  background-image: url();
+}
+.style2 {
+  font-family: Verdana, Arial, Helvetica, sans-serif;
+  color: #999999;
+  font-size: large;
+}
+.style4 {color: #999999; font-family: Verdana, Arial, Helvetica, sans-serif;
+  font-size: small;
+}
+.style1 {
+	font-family: Arial, Helvetica, sans-serif;
+}
+body,td,th {
+	font-family: Arial, Helvetica, sans-serif;
+}
+img{
+  border: 0
+}
+-->
+</style>
+
+</head>
+<body>
+  <div class="container">
+    <p align="center">&nbsp;</p>
+    <p align="center">&nbsp;</p>
+    <p align="center">&nbsp;</p>
+    <p align="center">&nbsp;</p>
+    <p align="center">&nbsp;</p>
+    <p align="center">&nbsp;</p>
+    <p align="center"><a href="http://www.relentlesshosting.com.au/" rel="nofollow"><img src="logo.png" alt="Relentless Hosting"></a><br></p>
+    <p align="center">&nbsp;</p>
+    <h2 align="center" class="style1">Under Construction</h2>
+  </div>
+</body>
+</html>
+
